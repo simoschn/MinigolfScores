41ed8807275be0e40ce9dd3abae1564204ed7ce9
simon-schneider@gmx.net <simon-schneider@gmx.net>
2015-05-05 17:41:00 +0200

    Started unit testing.

---
+++ app/src/main/java/test/UnitTest.java
@@ -0,0 +1,9 @@
+import android.test.InstrumentationTestCase;
+
+public class UnitTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+}
